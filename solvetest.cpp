5fc4f46395bd2756222725834dbd3cc6bee8a6b6
Hashimoto Junji <hasimoto@k-micro.com>
2012-11-15 09:36:23 +0900

    First Release

---
+++ solvetest.cpp
@@ -0,0 +1,51 @@
+#include "mat.h"
+
+
+template<class T>
+int
+test(){
+
+  Matrix<T> d(2,2);
+  Matrix<T> d2(2,2);
+
+  d<<
+    1,2,
+    3,4;
+
+  d2<<
+    3,4,
+    6,7;
+
+  d.write();
+  d2.write();
+  
+  (d+d).write();
+  d.write();
+  // (d+d2).write();
+  (d*d2).write();
+  (d*d2).write();
+  d.write();
+  d2.write();
+  (-d2).write();
+  (d-d).write();
+
+
+  Matrix<T> f(2,1);
+  f<<
+    1,
+    1;
+
+  f.write();
+  solve_gels(d,f).write();
+  solve_gels(d2,f).write();
+
+  return 0;
+}
+
+int
+main(){
+  test<double>();
+  test<float>();
+  test<int>();
+  return 0;
+}
